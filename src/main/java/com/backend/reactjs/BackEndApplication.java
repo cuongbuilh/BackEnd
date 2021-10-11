055cda074090e7a67c87f2aafca7b2776fb9257c
cuongbui <cuongbuilh@gmail.com>
2021-10-11 15:56:44 +0700

    add crossorigin

--- src/main/java/com/backend/reactjs/BackEndApplication.java
+++ src/main/java/com/backend/reactjs/BackEndApplication.java
@@ -2,8 +2,10 @@ package com.backend.reactjs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin("*")
 public class BackEndApplication {
 
     public static void main(String[] args) {
